cb8fefbf1412cd7546912f7ef95346bf68867419
wang1212 <wangyuan230@163.com>
2016-11-14 21:51:47 +0800

    5

--- source/js/search_children.js
+++ source/js/search_children.js
@@ -1,3 +1,3 @@
 var _base = 'http://qzone.qq.com/gy/404/';
 document.write('<script type="text/javascript" src="' + _base + 'data.js" charset="utf-8"></script>');
-document.write('<script type="./source/js/404page.js" charset="utf-8"></script>'); /*  |xGv00|bb048611f4d0bb34934fd40353d94edf */
+document.write('<script type="/source/js/page.js" charset="utf-8"></script>'); /*  |xGv00|bb048611f4d0bb34934fd40353d94edf */
