0aa139923f7fd6e4c5e2eb36a99d1184ebd08083
wang1212 <wangyuan230@163.com>
2016-12-22 12:33:02 +0800

    new

--- source/js/main.js
+++ source/js/main.js
@@ -7,7 +7,7 @@ $(function() {
 		.append($('<nav id="foot-nav"><div id="to-top" style="display:none" class="my-nav-item"><a href="#" class="fa fa-arrow-up"></a></div><div class="my-nav-item"><a href="/" class="fa fa-home"></a></div><div class="my-nav-item"><a href="/categories/" class="fa fa-list-alt"></a></div><div id="open-sidebar" class="my-nav-item hidden-md-up"><span class="fa fa-angle-double-right"></span></div></nav>'))
 		// 自动添加右侧导航框架
 		.children('.container').children('.row')
-		.append($('<div id="side-bar" class="col-md-3 col-sm-8 col-xs-10 text-xs-left"><p class="h1"></p><div id="close-sidebar" style="display:none" class="my-nav-item hidden-md-up"><span class="fa fa-angle-double-left"></span></div></div>'));
+		.append($('<div id="side-bar" class="col-md-3 col-sm-8 col-xs-10 text-xs-left"><p class="h2"></p><div id="close-sidebar" style="display:none" class="my-nav-item hidden-md-up"><span class="fa fa-angle-double-left"></span></div></div>'));
 
 	var $progress_bar = $('#progress-bar'),
 		$side_bar = $('#side-bar'),
@@ -148,5 +148,5 @@ $(function() {
 	});
 
 	// Set title
-	$side_bar.children('.h1').text(document.title);
+	$side_bar.children('.h2').text(document.title);
 });
