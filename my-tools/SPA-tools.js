a3692bf61ff30aa0bf26ab2fb16917a4a02b8dab
MrWang <wangyuan230@163.com>
2017-12-30 14:53:59 +0800

    update spa-tool

--- my-tools/SPA-tools.js
+++ my-tools/SPA-tools.js
@@ -1,4 +1,9 @@
-// IE8 以下不支持的数组方法
+
+'use strict';
+
+/*
+	IE8 以下不支持的数组方法
+ */
 if (!Array.prototype.forEach) {
 	Array.prototype.forEach = function(callback, self){
 		if (Object.prototype.toString.call(callback) != "[object Function]") {
@@ -28,6 +33,83 @@ if (!Array.prototype.filter) {
 		return this;
 	}
 }
+
+/*
+	数组（Array）扩展类方法 -ex 前缀（extend）
+ */
+/*  多个数组相互重合的元素（类方法）  */
+Array._ex_arrays_overlap = function(arrays){
+	if (!arrays || arrays.length < 2) {
+		return [];
+	}
+	// 找出最短数组
+	var _min_index = 0;
+
+	for (var i = 1; i < arrays.length; i++) {
+		_min_index = arrays[_min_index].length < arrays[i].length ? _min_index : i;
+	}
+
+	// 找出数组之间重合的元素
+	var _overlaps = [];
+
+	var _min_array = arrays.splice(_min_index, 1)[0];
+
+	_min_array.forEach(function(item, index){
+		var _flag = true;
+
+		for (var i = 0; i < arrays.length; i++) {
+			if (arrays[i].indexOf(item) == -1) {
+				_flag = false;
+				break;
+			}
+		}
+
+		if (_flag) {
+			_overlaps.push(item);
+		}
+	});
+	// 以数组形式返回
+	return _overlaps._ex_delete_repeat();
+}
+
+/*
+	数组（Array）扩展实例方法 -ex 前缀（extend）
+ */
+/* 删除数组中的值（实例方法） */
+Array.prototype._ex_delete = function(value){
+	var _self = this;
+
+	if (Object.prototype.toString.call(value) == "[object Array]") {
+		return value.forEach(function(item, index){
+			_self._ex_delete(item)
+		});
+	}
+
+	var _index = -1;
+
+	// 判断在数组中则删除（重复删除）
+	while(_self.indexOf(value) != -1){
+		_index = _self.indexOf(value);
+
+		_self.splice(_index, 1);
+	}
+
+	// 返回索引
+	return _index;
+}
+/* 删除数组中重复的值（实例方法） */
+Array.prototype._ex_delete_repeat = function(){
+	var _no_repeat = [];
+
+	this.forEach(function(item, index){
+		if (_no_repeat.indexOf(item) == -1) {
+			_no_repeat.push(item);
+		}
+	});
+
+	return _no_repeat;
+}
+/*   */
 
 /*
 	数据库、监视器（管理全局数据，数据改动触发相应事件）   wangyuan
@@ -136,6 +218,212 @@ Model.prototype = {
 		return true;		
 	}
 }
+
+
+/*
+	Component 组件系统 ------------------------------------------------------------------
+ */
+!function(window){
+
+	// 构造器
+	function Component(options){
+		var _self = this,
+			_default_options = {
+				'id': null,            //（类型：字符串）单例引用，有此属性值时组件使用单例模式
+				'dom': null,           //（类型：字符串）DOM 结构
+				'props': null,		   //（类型：数组）属性，以 prop-id 形式在元素类名中声明，渲染时改变对应元素的属性 [{'id'： 1, text': 'hello', class': 'factor'}]
+				'datas': null,         //（类型：任意）数据，挂载到组件上
+				'events': null,        //（类型：方法）绑定事件的操作
+				'before': null,		   //（类型：方法）DOM 挂载在 target 之前的操作
+				'init': null,          //（类型：方法）初始化时的操作
+				'update': null,  	   //（类型：方法）更新时的操作
+				'target': $('body')    //（类型：jQuery 对象）挂载的父级元素
+			};
+
+		(_self._id = options.id || _default_options.id) && (C._singleton[_self._id] = _self);
+
+		_self.dom = options.dom && $(options.dom) || _default_options.dom;
+
+		_self._props = options.props || _default_options.props;
+
+		_self.datas = options.datas || _default_options.datas;
+
+		_self._event = { 'events': options.events || _default_options.events };
+		
+		_self._before = options.before || _default_options.before;
+
+		_self._init = options.init || _default_options.init;
+
+		_self._update = options.update || _default_options.update;
+
+		// 显示（重写）
+		_self.show = options.show || _self.show;
+
+		// 关闭（重写）
+		_self.close = options.close || _self.close;
+
+		// 销毁（重写）
+		_self.destory = options.destory || _self.destory;
+
+		// 挂载前操作
+		_self.before();
+
+		// 挂载到目标元素上
+		_self.dom && _self.dom.appendTo(options.target || _default_options.target);
+
+		// 自动渲染
+		(options.render != false) && _self.render();
+	}
+	Component.prototype = {
+		'constructor': Component,
+
+		'show': function(callback){
+
+			var _self = this;
+
+			_self.dom && _self.dom.fadeIn(function(){
+				if (callback && Object.prototype.toString.call(callback) == "[object Function]") {
+					callback();
+				}
+			});
+
+			return _self;
+		},
+
+		'close': function(callback){
+
+			var _self = this;
+
+			_self.dom && _self.dom.fadeOut(function(){
+				if (callback && Object.prototype.toString.call(callback) == "[object Function]") {
+					callback();
+				}
+			});
+
+			return _self;	
+		},
+
+		'destory': function(callback){
+
+			var _self = this;
+
+			_self.close(function(){
+				_self.dom.remove();
+				if (callback && Object.prototype.toString.call(callback) == "[object Function]") {
+					callback();
+				}
+				// 单例模式下销毁实例
+				_self._id && (delete C._singleton[_self._id]);
+			});
+		},
+
+		'before': function(){
+
+			var _self = this;
+
+			if (_self._before && Object.prototype.toString.call(_self._before) == "[object Function]") {
+				_self._before.apply(_self, [].slice.call(arguments));
+
+				delete _self._before;  // 下次不再重复
+			}	
+		},
+
+		// 渲染
+		'render': function(){
+
+			var _self = this;
+
+			// 渲染属性
+			_self.dom && _self._props && _self._props.forEach(function(item, index){
+				var _class = '.prop-' + item.id,
+					_$elem = _self.dom.is(_class) && _self.dom || _self.dom.find(_class);
+
+				for(var _type in item){
+					switch(_type){
+						case 'text':
+							_$elem.text(item[_type] || '');
+							break;
+						case 'html':
+							_$elem.html(item[_type] || '');
+							break;
+						case 'class':
+							_$elem.addClass(item[_type] || '');
+							break;
+						case 'attr':
+							if (Object.prototype.toString.call(item[_type]) == "[object Array]") {
+								item[_type].forEach(function(attr, index){
+									_$elem.attr(attr.k, attr.v || null);
+								});
+							} else {
+								_$elem.attr(item[_type].k, item[_type].v || null);
+							}
+							break;
+						case 'data':
+							if (Object.prototype.toString.call(item[_type]) == "[object Array]") {
+								item[_type].forEach(function(data, index){
+									_$elem.data(data.k, data.v || null);
+								});
+							} else {
+								_$elem.data(item[_type].k, item[_type].v || null);
+							}		
+					}
+				}	
+			});
+
+			/*
+				事件绑定（只绑定一次）
+			 */
+			if (_self._event) {
+				// 事件绑定
+				if (_self._event.events && Object.prototype.toString.call(_self._event.events) == "[object Function]") {
+					_self._event.events.apply(_self, [].slice.call(arguments));
+				}			
+				// 组件关闭按钮事件
+				_self.dom.find('.component-close').bind('click', function(){
+					_self.close()
+				});
+
+				// 组件销毁按钮事件
+				_self.dom.find('.component-destory').bind('click', function(){
+					_self.destory();
+				});
+
+				delete _self._event;  // 下次渲染不再重复绑定事件
+			}
+
+			// 初始化（只执行一次）
+			if (_self._init && Object.prototype.toString.call(_self._init) == "[object Function]") {
+				_self._init.apply(_self, [].slice.call(arguments));
+
+				delete _self._init;  // 下次渲染不再重复初始化
+			}
+
+			return _self;
+		},
+
+		// 更新
+		'update': function(){
+
+			var _self = this;
+			
+			if (_self._update && Object.prototype.toString.call(_self._update) == "[object Function]") {
+				_self._update.apply(_self, [].slice.call(arguments));
+			}
+
+			return _self;
+		}
+	}
+
+	var C = {
+		// 单例容器
+		"_singleton": {},
+		// 构造器
+		'_Component': Component
+	}
+
+	window.Component = C;
+
+}(window);
 
 
 /*  
