6e910047e68d0b17023c0dc2878aa7f3b81dbdf3
wang1212 <mrwang1212@126.com>
2019-05-26 23:35:21 +0800

    build v2.0

---
+++ build/service-worker.js
@@ -0,0 +1,45 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("wb-assets/workbox-v4.3.1/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "wb-assets/workbox-v4.3.1"});
+
+importScripts(
+  "wb-assets/precache-manifest.58e62f23d7d6ee3dc71382943edf958d.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "vendor-manifest.json",
+    "revision": "171868a25370f087cefb74dbfec288c9"
+  },
+  {
+    "url": "vendor.js",
+    "revision": "c37970d9378941237e2151bae0ae71cd"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerRoute(/^https:\/\/fonts\.googleapis\.com/, new workbox.strategies.StaleWhileRevalidate({ "cacheName":"google-fonts-stylesheets", plugins: [] }), 'GET');
+workbox.routing.registerRoute(/^https:\/\/fonts\.gstatic\.com/, new workbox.strategies.CacheFirst({ "cacheName":"google-fonts-webfonts", plugins: [new workbox.cacheableResponse.Plugin({ statuses: [ 0, 200 ] }), new workbox.expiration.Plugin({ maxAgeSeconds: 31536000, purgeOnQuotaError: false })] }), 'GET');
---
+++ build/wb-assets/precache-manifest.58e62f23d7d6ee3dc71382943edf958d.js
@@ -0,0 +1,120 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "952fbbee65dc5624202f91ce114d102d",
+    "url": "../index.html"
+  },
+  {
+    "url": "app.ac8b62d53732f8b70aa2.css"
+  },
+  {
+    "url": "app.e27a50c71e8fd73424aa.js"
+  },
+  {
+    "revision": "9b053b121e306b1e6199b35935593451",
+    "url": "favicon.ico"
+  },
+  {
+    "revision": "269906e94c697f6fbc3a60b15b10ef0a",
+    "url": "favicon.png"
+  },
+  {
+    "revision": "e0c61ef21b27bd2bbfa594a9038449ef",
+    "url": "favicon_512.png"
+  },
+  {
+    "revision": "460002d0891dbc77e5ae063df61f7389",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "09c162faaf4eedc90bf958d662c12df9",
+    "url": "tmp_index.html"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "vendors\\fonts\\.gitkeep"
+  },
+  {
+    "revision": "c4cf38a1be22c3d8e660e00bd307c436",
+    "url": "vendors\\libs\\bootstrap-4.3.1-dist\\css\\bootstrap-grid.css"
+  },
+  {
+    "revision": "3654b807c094151ff379c873f462b1da",
+    "url": "vendors\\libs\\bootstrap-4.3.1-dist\\css\\bootstrap-grid.min.css"
+  },
+  {
+    "revision": "a6cd05cb930cdf828abf6a6ae37e3821",
+    "url": "vendors\\libs\\bootstrap-4.3.1-dist\\css\\bootstrap-reboot.css"
+  },
+  {
+    "revision": "db63058f8ae01765f995eab3e7a3688d",
+    "url": "vendors\\libs\\bootstrap-4.3.1-dist\\css\\bootstrap-reboot.min.css"
+  },
+  {
+    "revision": "c496036354fa5512674ddd744e845edb",
+    "url": "vendors\\libs\\bootstrap-4.3.1-dist\\css\\bootstrap.css"
+  },
+  {
+    "revision": "8fe70898895271ddc62823321011273a",
+    "url": "vendors\\libs\\bootstrap-4.3.1-dist\\css\\bootstrap.min.css"
+  },
+  {
+    "revision": "bd1604da466f7535f07899082f104eec",
+    "url": "vendors\\libs\\bootstrap-4.3.1-dist\\js\\bootstrap.bundle.js"
+  },
+  {
+    "revision": "b41fe9374205bd087a4d4f0ab5a195be",
+    "url": "vendors\\libs\\bootstrap-4.3.1-dist\\js\\bootstrap.bundle.min.js"
+  },
+  {
+    "revision": "5e7d168ed3203dab385e83f97f98f725",
+    "url": "vendors\\libs\\bootstrap-4.3.1-dist\\js\\bootstrap.js"
+  },
+  {
+    "revision": "0a958254db529f99f475080fe2a6dcdb",
+    "url": "vendors\\libs\\bootstrap-4.3.1-dist\\js\\bootstrap.min.js"
+  },
+  {
+    "revision": "c6dc970d32f522a33bcd752dd8d28bb5",
+    "url": "vendors\\libs\\material-design-icons-3.0.1\\material-icons.css"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "vendors\\libs\\material-design-icons-3.0.1\\webfonts\\MaterialIcons-Regular.eot"
+  },
+  {
+    "revision": "ed6a98d002bc0b535dd8618f3ae05fe7",
+    "url": "vendors\\libs\\material-design-icons-3.0.1\\webfonts\\MaterialIcons-Regular.ijmap"
+  },
+  {
+    "revision": "1c18379e0279731596f8263d94389a72",
+    "url": "vendors\\libs\\material-design-icons-3.0.1\\webfonts\\MaterialIcons-Regular.svg"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "vendors\\libs\\material-design-icons-3.0.1\\webfonts\\MaterialIcons-Regular.ttf"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "vendors\\libs\\material-design-icons-3.0.1\\webfonts\\MaterialIcons-Regular.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "vendors\\libs\\material-design-icons-3.0.1\\webfonts\\MaterialIcons-Regular.woff2"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "vendors\\plugins\\.gitkeep"
+  },
+  {
+    "revision": "7b7a4e3a218061d489d18edc20018200",
+    "url": "vendors\\plugins\\compatible-plugin\\html5shiv.min.js"
+  },
+  {
+    "revision": "43f8e67af3194f1791c6e415195b6ca8",
+    "url": "vendors\\plugins\\compatible-plugin\\respond.min.js"
+  },
+  {
+    "revision": "8f5374e4cb3724b206cdf601516c5483",
+    "url": "vendors\\plugins\\compatible-plugin\\selectivizr.min.js"
+  }
+]);
